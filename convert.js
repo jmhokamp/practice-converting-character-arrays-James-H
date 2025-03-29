17a977831584162ef359594b78298a375d7166c8
jhokamp <jmhokamp@gmail.com>
2025-03-28 21:47:49 -0500

    finalupdate

--- convert.js
+++ convert.js
@@ -5,21 +5,26 @@
 let message1 =  " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
 let decodedMess1 = message1.split('').reverse().join('');
 console.log(decodedMess1);
+//Coding may seem hard at first, but if you persist, you'll get it! You can also use coding skills in a variety of settings! Keep going, and you'll be okay!
+
 
 //Message 2: "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB" 
 let message2 =  "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
 let decodedMess2 = message2.split('').reverse().join('');
 console.log(decodedMess2);
+//But code writing will feel hard at first. Take time, you‘ll get better with practice!
 
 //Message 3: "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY"
 let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY"; 
 let decodedMess3 = message3.split('').reverse().join('');
 console.log(decodedMess3);
+//You don't have to feel like a professional. If you have trouble, you are no different from other people!
 
 //Message 4: ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ" 
 let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
 let decodedMess4 = message4.split('').reverse().join('');
 console.log(decodedMess4);
+//Just start today. Don't wait for the perfect moment. If you can only write small programs, they still matter.
 
 //Task 2: Write your own reverse messages
 
@@ -30,12 +35,16 @@ let myMessage4 = "You will fail, but you will learn and grow from this!"
 
 let messageRevers1 = myMessage1.split('').reverse().join('');
 console.log(messageRevers1);
+//!deeccus lliw uoY .ti ekam ot gniog er'uoy tub ,hguot eb ot gniog s'tI
 
 let messageRevers2 = myMessage2.split('').reverse().join('');
 console.log(messageRevers2);
+//!enigami uoy naht gnorts era uoy ,flesruoy ni eveileB
 
 let messageRevers3 = myMessage3.split('').reverse().join('');
 console.log(messageRevers3);
+//!taerg od ll'uoy ,wen gnihtemos gniyrt rof uoy fo duorp os ma I
 
 let messageRevers4 = myMessage4.split('').reverse().join('');
 console.log(messageRevers4);
+//!siht morf worg dna nrael lliw uoy tub ,liaf lliw uoY
